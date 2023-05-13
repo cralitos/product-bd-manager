c9f88dc53118265528306c46fbdc5557d114b485
Carlos Hernandez <cralitos@gmail.com>
2023-05-13 10:39:04 -0600

    first commit

--- src/main/java/com/example/productmanager/ProductManagerApplication.java
+++ src/main/java/com/example/productmanager/ProductManagerApplication.java
@@ -2,12 +2,37 @@ package com.example.productmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ProductManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManagerApplication.class, args);
 	}
 
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.example"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfoBuilder()
+				.title("Api productos")
+				.contact(new Contact("Carlos Hernandez","no-url","my-email"))
+				.description("demo documentacion")
+				.build();
+	}
 }
